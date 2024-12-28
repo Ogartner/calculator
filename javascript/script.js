bdb0df4e050ca1e5e3961b4fa484d88926ace279
Ogartner <ogartner94@gmail.com>
2024-12-28 16:50:10 +0100

    Add basic calculations to calculator, shows on display.

--- javascript/script.js
+++ javascript/script.js
@@ -1,7 +1,14 @@
 // GLOBAL
+const display = document.querySelector('.display');
 const displayContent = document.querySelector('.display-content');
 const numPad = document.querySelectorAll('ul');
-const num = document.querySelectorAll('.num');
+const numBtn = document.querySelectorAll('.num');
+const operatorBtn = document.querySelectorAll('.operator');
+const equalBtn = document.querySelector('.equal-btn');
+
+let num1;
+let num2;
+let operator;
 
 // Addition
 const add = (num1, num2) => {
@@ -39,14 +46,62 @@ const operate = (num1, num2, operator) => {
   }
 };
 
-num.forEach((element, i) =>
-  element.addEventListener('click', (e) => {
-    if (i === 9) {
-      displayContent.innerHTML = 0;
-    } else {
-      displayContent.innerHTML = i + 1;
-    }
-  })
-);
+const pressNumBtn = () => {
+  numBtn.forEach((element, i) => {
+    element.addEventListener('click', (e) => {
+      if (i === 9) {
+        displayContent.innerHTML += 0;
+      } else {
+        displayContent.innerHTML += i + 1;
+      }
+    });
+  });
+};
 
-console.log(operate(8, 4, '*'));
+const pressOperatorBtn = () => {
+  operatorBtn.forEach((element, i) => {
+    element.addEventListener('click', (e) => {
+      num1 = Number(displayContent.innerHTML);
+      setTimeout(() => {
+        display.classList.toggle('flash');
+      }, 250);
+      displayContent.innerHTML = '';
+      display.classList.toggle('flash');
+
+      switch (i) {
+        case 0:
+          operator = '+';
+          break;
+        case 1:
+          operator = '-';
+          break;
+        case 2:
+          operator = '*';
+          break;
+        case 3:
+          operator = '/';
+          break;
+      }
+    });
+  });
+};
+
+const pressEqualBtn = () => {
+  equalBtn.addEventListener('click', () => {
+    num2 = Number(displayContent.innerHTML);
+    setTimeout(() => {
+      display.classList.toggle('flash-b');
+    }, 250);
+    display.classList.toggle('flash-b');
+    num1 = operate(num1, num2, operator);
+    displayContent.innerHTML = `${num1}`;
+  });
+};
+
+const init = () => {
+  pressNumBtn();
+  pressOperatorBtn();
+  pressEqualBtn();
+};
+
+init();
